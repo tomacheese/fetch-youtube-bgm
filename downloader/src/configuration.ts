9c23c95b9c8511e162ba6a8800c720a62b72ba20
Tomachi <8929706+book000@users.noreply.github.com>
2023-03-07 06:36:54 +0900

    feat: supported discord notify (#178)

--- downloader/src/configuration.ts
+++ downloader/src/configuration.ts
@@ -1,7 +1,16 @@
 import fs from 'fs'
 
-interface Config {
+export interface Config {
   playlistId: string
+  /** Discord webhook URL or bot token */
+  discord: {
+    /** Discord webhook URL (required if using webhook) */
+    webhook_url?: string
+    /** Discord bot token (required if using bot) */
+    token?: string
+    /** Discord channel ID (required if using bot) */
+    channel_id?: string
+  }
 }
 
 export function getConfig() {
